83ff997ff62070d961e8093f31f2b0bd5880605a
Skalyaeve <anguinau@student.42.fr>
2023-09-23 16:56:34 +0200

    Wororked on Arachnida

--- 00-Arachnida/00-Spider/TkImgViewer.py
+++ 00-Arachnida/00-Spider/TkImgViewer.py
@@ -1,89 +1,133 @@
 from os.path import basename
 from PIL.Image import open as PIL_open
 from PIL.ImageTk import PhotoImage
-from tkinter import Tk, Toplevel, Frame, Canvas, Label, Widget
+from tkinter import Tk, Toplevel, Frame, Canvas, Label, Widget, Scrollbar
 from PIL.ImageSequence import Iterator as PIL_iterator
 import pyexiv2
+
 
 class TkImgViewer:
     def __init__(self, img_path: str, parent=None):
         self.parent = parent
-        self.loadImgData(img_path)
-        self.loadScreen()
+        self.load_img_data(img_path)
+        self.load_screen()
 
-    def loadScreen(self):
-        try:
-            self.window = Tk() if not self.parent else Toplevel(self.parent)
-            self.window.title("TkImgViewer")
-            self.__load_infos()
-            self.__load_img()
-            self.window.mainloop()
-        except Exception as e:
-            print(f"TkImgViewer: [ERROR] loadScreen(): ", e)
+    def load_screen(self):
+        if not self.img:
+            print("TkImgViewer: [ERROR] load_screen(): please load an image first")
+            return
+        self.window = Tk() if not self.parent else Toplevel(self.parent)
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
+        self.img_width = self.img.width if self.img.width < screen_width - 400 else screen_width - 400
+        self.img_height = self.img.height if self.img.height < screen_height - 150 else screen_height - 150
 
-    def loadImgData(self, img_path: str):
+        self.window.title("TkImgViewer")
+        self.__load_infos()
+        self.__load_img()
+        self.window.mainloop()
+
+    def load_img_data(self, img_path: str):
         try:
             self.img_path = img_path
             metadata = pyexiv2.Image(self.img_path)
             try:
-                self.exif = []
-                exif_data = metadata.read_exif()
-                for key, value in exif_data.items():
-                    self.exif.append((key, value))
+                try:
+                    self.exif = []
+                    exif_data = metadata.read_exif()
+                    for key, value in exif_data.items():
+                        self.exif.append((key, value))
+                except Exception as e:
+                    pass
 
-                self.iptc = []
-                iptc_data = metadata.read_iptc()
-                for key, value in iptc_data.items():
-                    self.iptc.append((key, value))
+                try:
+                    self.iptc = []
+                    iptc_data = metadata.read_iptc()
+                    for key, value in iptc_data.items():
+                        self.iptc.append((key, value))
+                except Exception as e:
+                    pass
 
-                self.xmp = []
-                xmp_data = metadata.read_xmp()
-                for key, value in xmp_data.items():
-                    self.xmp.append((key, value))
+                try:
+                    self.xmp = []
+                    xmp_data = metadata.read_xmp()
+                    for key, value in xmp_data.items():
+                        self.xmp.append((key, value))
+                except Exception as e:
+                    pass
 
                 metadata.close()
                 self.img = PIL_open(self.img_path)
             except Exception as e:
-                print("TkImgViewer: loadImgData(): can't get metadata: ", e)
+                print("TkImgViewer: load_img_data(): can't get metadata: ", e)
         except Exception as e:
-            print(f"TkImgViewer: [ERROR] loadImgData():" , e)
+            print(f"TkImgViewer: [ERROR] load_img_data(): ", e)
 
     def __load_infos(self):
-        left_frame = Frame(self.window)
-        left_frame.pack(side="left", fill="y", expand=True)
-        canvas = Canvas(self.window, width=self.img.width, height=self.img.height)
-        canvas.pack(side="left")
-        metadata_label = Label(left_frame, text=f"{basename(self.img_path)}:", justify="left")
-        metadata_label.pack(anchor="w")
-        size_label = Label(left_frame, text=f"Size: {self.img.size}", justify="left")
+        frame_width = 300
+        left_frame = Frame(self.window, width=frame_width, height=self.img_height)
+        left_frame.pack(side="left", fill="y")
+
+        x_scrollbar = Scrollbar(left_frame, orient="horizontal")
+        x_scrollbar.pack(side="bottom", fill="x")
+        y_scrollbar = Scrollbar(left_frame, orient="vertical")
+        y_scrollbar.pack(side="right", fill="y")
+
+        info_canvas = Canvas(left_frame, width=frame_width, height=self.img_height)
+        info_canvas.config(xscrollcommand=x_scrollbar.set, yscrollcommand=y_scrollbar.set)
+        info_canvas.pack(side="left", fill="y")
+
+        x_scrollbar.config(command=info_canvas.xview)
+        y_scrollbar.config(command=info_canvas.yview)
+
+        content_frame = Frame(info_canvas)
+        info_canvas.create_window((0, 0), window=content_frame, anchor="nw")
+
+        filename_label = Label(content_frame, text=f"{basename(self.img_path)}:", justify="left")
+        filename_label.pack(anchor="w")
+
+        size_label = Label(content_frame, text=f"Size: {self.img.size}", justify="left")
         size_label.pack(anchor="w")
+
         if self.exif:
             for x, y in self.exif:
-                data_label = Label(left_frame, text=f"{x}: {y}", justify="left")
+                data_label = Label(content_frame, text=f"{x}: {y}", justify="left")
                 data_label.pack(anchor="w")
         if self.iptc:
             for x, y in self.iptc:
-                data_label = Label(left_frame, text=f"{x}: {y}", justify="left")
+                data_label = Label(content_frame, text=f"{x}: {y}", justify="left")
                 data_label.pack(anchor="w")
         if self.xmp:
             for x, y in self.xmp:
-                data_label = Label(left_frame, text=f"{x}: {y}", justify="left")
+                data_label = Label(content_frame, text=f"{x}: {y}", justify="left")
                 data_label.pack(anchor="w")
 
-    def __load_img(self):
-        img_width, img_height = self.img.size
-        canvas = self.window.winfo_children()[1]
-        canvas.config(width=img_width, height=img_height)
-        if self.img.format != "GIF":
-            self.__display_static_img(canvas)
-        else:
-            self.__display_animated_img(canvas)
+        content_frame.update_idletasks()
+        info_canvas.config(scrollregion=info_canvas.bbox("all"))
+        if self.exif or self.iptc or self.xmp:
+            data_label.pack(anchor="w")
 
-    def __display_static_img(self, canvas: Widget):
-        img_width, img_height = self.img.size
-        canvas.config(width=img_width, height=img_height)
-        self.displayed_img = PhotoImage(self.img)
-        canvas.create_image(0, 0, anchor="nw", image=self.displayed_img)
+    def __load_img(self):
+        frame = Frame(self.window)
+        frame.pack(side="left")
+
+        x_scroll = Scrollbar(frame, orient="horizontal")
+        x_scroll.pack(side="bottom", fill="x")
+        y_scroll = Scrollbar(frame, orient="vertical")
+        y_scroll.pack(side="right", fill="y")
+
+        img_canvas = Canvas(frame, width=self.img_width, height=self.img_height, scrollregion=(0, 0, self.img.width, self.img.height))
+        img_canvas.pack(side="left")
+
+        img_canvas.config(xscrollcommand=x_scroll.set, yscrollcommand=y_scroll.set)
+        x_scroll.config(command=img_canvas.xview)
+        y_scroll.config(command=img_canvas.yview)
+
+        if self.img.format != "GIF":
+            self.displayed_img = PhotoImage(self.img)
+            img_canvas.create_image(0, 0, anchor="nw", image=self.displayed_img)
+        else:
+            self.__display_animated_img(img_canvas)
 
     def __display_animated_img(self, canvas: Widget):
         frames = []
--- 00-Arachnida/01-Scorpion/scorpion.py
+++ 00-Arachnida/01-Scorpion/scorpion.py
@@ -1,31 +1,299 @@
-from argparse import ArgumentParser
+from os.path import basename as os_path_basename
+from PIL import Image as PIL_Image
+from PIL.ImageSequence import Iterator as PIL_ImageSequence_Iterator
+from PIL.ImageTk import PhotoImage as PIL_ImageTk_PhotoImage
+from tkinter import Tk as tkinter_Tk
+from tkinter import Toplevel as tkinter_Toplevel
+from tkinter import Frame as tkinter_Frame
+from tkinter import Canvas as tkinter_Canvas
+from tkinter import Label as tkinter_Label
+from tkinter import Scrollbar as tkinter_Scrollbar
+from pyexiv2 import Image as pyexiv2_Image
 
-from ExifManager import ExifManager
 
-c_red = "\033[91m"
-c_stop = "\033[0m"
+class Scorpion:
+    def __init__(self, img_path: str):
+        self.load_data(img_path)
 
-if __name__ == "__main__":
-    parser = ArgumentParser(description="Display image metadata")
-    parser.add_argument("images", type=str, nargs="+", help="an image to analyze")
-    parser.add_argument("-m", metavar="TAG=VALUE", help="modify specific EXIF data (e.g., -m TAG=VALUE)")
-    parser.add_argument("-d", metavar="TAG", help="delete specific EXIF data (e.g., -d TAG)")
-    parser.add_argument("-g", action="store_true", help="Show the graphic interface")
-    args = parser.parse_args()
+    def load_data(self, img_path: str):
+        if not img_path:
+            print(f"Scorpion: [ ERROR ] load_data({img_path}):\nNo image path provided.")
+            return
 
-    allowedExt = (".jpeg", ".jpg", ".png", ".gif", ".bmp")
-    for img_path in args.images:
-        if not any(img_path.endswith(ext) for ext in allowedExt):
-            print(f"scorpion: [ERROR] {img_path}:", c_red + "extension not supported" + c_stop)
-            continue
+        self.path = img_path
+        self.width = None
+        self.height = None
+        self.format = None
+        self.exif = {}
+        self.iptc = {}
+        self.xmp = {}
 
-        manager = ExifManager(img_path)
-        if args.d:
-            manager.delete(args.d)
-        if args.m:
+        try:
+            img = PIL_Image.open(self.path)
+            self.width = img.width
+            self.height = img.height
+            self.format = img.format
+            img.close()
+        except Exception as e:
+            print(f"Scorpion: [ ERROR ] load_data({img_path}):\nPIL_Image_open({self.path}): {e}")
+
+        try:
+            metadata = pyexiv2_Image(self.path)
             try:
-                key, value = args.m.split("=", 2)
-                manager.modify(key, value)
-            except:
-                print(f"scorpion: [ERROR] invalid input for -m: {args.m}")
-        manager.show(args.g)
+                self.exif = metadata.read_exif()
+            except Exception as e:
+                print(f"Scorpion: [ ERROR ] load_data({img_path}):\nmetadata.read_exif(): {e}")
+
+            try:
+                self.iptc = metadata.read_iptc()
+            except Exception as e:
+                print(f"Scorpion: [ ERROR ] load_data({img_path}):\nmetadata.read_iptc(): {e}")
+
+            try:
+                self.xmp = metadata.read_xmp()
+            except Exception as e:
+                print(f"Scorpion: [ ERROR ] load_data({img_path}):\nmetadata.read_xmp(): {e}")
+
+            metadata.close()
+        except Exception as e:
+            print(f"Scorpion: [ ERROR ] load_data({img_path}):\npyexiv2_Image({self.path}): {e}")
+
+    def show_metadata(self, graphic: bool = False):
+        if not self.path:
+            print(f"Scorpion: [ ERROR ] show_metadata():\nPlease load_data() first.")
+            return
+
+        if graphic:
+            return self.__load_gui()
+
+        print("------------------ S C O R P I O N ------------------")
+        print(f"Filename: {os_path_basename(self.path)}")
+        print(f"Width: {self.width}")
+        print(f"Height: {self.height}")
+
+        if len(self.exif):
+            print("--------- E X I F ---------")
+            for x, y in self.exif.items():
+                print(f"{x}: {y}")
+
+        if len(self.iptc):
+            print("--------- I P T C ---------")
+            for x, y in self.iptc.items():
+                print(f"{x}: {y}")
+
+        if len(self.xmp):
+            print("---------- X M P ----------")
+            for x, y in self.xmp.items():
+                print(f"{x}: {y}")
+        print("-----------------------------------------------------")
+
+    def modify_metadata(self, key: str, value: str):
+        if not self.path:
+            print(f"Scorpion: [ ERROR ] modify_metadata({key}, {value}):\nPlease load_data() first.")
+            return
+        if not key:
+            print(f"Scorpion: [ ERROR ] modify_metadata({key}, {value}):\nNo key provided.")
+            return
+
+        try:
+            metadata = pyexiv2_Image(self.path)
+            metadata_type = key.split(".")[0]
+
+            if metadata_type == "Exif":
+                self.exif[key] = value
+                try:
+                    metadata.modify_exif(self.exif)
+                except Exception as e:
+                    print(f"Scorpion: [ ERROR ] modify_metadata({key}, {value}):\nmetadata.modify_exif(): {e}")
+
+            elif metadata_type == "Iptc":
+                self.iptc[key] = value
+                try:
+                    metadata.modify_iptc(self.iptc)
+                except Exception as e:
+                    print(f"Scorpion: [ ERROR ] modify_metadata({key}, {value}):\nmetadata.modify_iptc(): {e}")
+
+            elif metadata_type == "Xmp":
+                self.xmp[key] = value
+                try:
+                    metadata.modify_xmp(self.xmp)
+                except Exception as e:
+                    print(f"Scorpion: [ ERROR ] modify_metadata({key}, {value}):\nmetadata.modify_xmp(): {e}")
+
+            metadata.close()
+        except Exception as e:
+            print(f"Scorpion: [ ERROR ] modify_metadata({key}, {value}):\npyexiv2_Image({self.path}): {e}")
+
+    def delete_metadata(self, key: str):
+        if not self.path:
+            print(f"Scorpion: [ ERROR ] delete_metadata({key}):\nPlease load_data() first.")
+            return
+        if not key:
+            raise (f"Scorpion: [ ERROR ] delete_metadata({key}):\nNo key provided.")
+
+        if self.has_metadata(key):
+            try:
+                metadata = pyexiv2_Image(self.path)
+                metadata_type = key.split(".")[0]
+
+                if metadata_type == "Exif":
+                    del self.exif[key]
+                    try:
+                        metadata.modify_exif(self.exif)
+                    except Exception as e:
+                        print(f"Scorpion: [ ERROR ] delete_metadata({key}):\nmetadata.modify_exif(): {e}")
+
+                elif metadata_type == "Iptc":
+                    del self.iptc[key]
+                    try:
+                        metadata.modify_iptc(self.iptc)
+                    except Exception as e:
+                        print(f"Scorpion: [ ERROR ] delete_metadata({key}):\nmetadata.modify_iptc(): {e}")
+
+                elif metadata_type == "Xmp":
+                    del self.xmp[key]
+                    try:
+                        metadata.modify_xmp(self.xmp)
+                    except Exception as e:
+                        print(f"Scorpion: [ ERROR ] delete_metadata({key}):\nmetadata.modify_xmp(): {e}")
+
+                metadata.close()
+            except Exception as e:
+                print(f"Scorpion: [ ERROR ] delete_metadata({key}):\npyexiv2_Image({self.path}): {e}")
+        else:
+            print(f"Scorpion: [ ERROR ] delete_metadata({key}):\n{key} not found.")
+
+    def has_metadata(self, key: str):
+        if not self.path:
+            print(f"Scorpion: [ ERROR ] delete_metadata({key}):\nPlease load_data() first.")
+            return
+        if not key:
+            print(f"Scorpion: [ ERROR ] delete_metadata({key}):\nNo key provided.")
+            return
+
+        for metadata in [self.exif, self.iptc, self.xmp]:
+            if key in metadata:
+                return True
+        return False
+
+    def __load_gui(self, parent=None):
+        self.gui = {
+            "parent": parent,
+            "window": None,
+            "screen_width": None,
+            "screen_height": None,
+            "img": None,
+        }
+        try:
+            self.gui["window"] = tkinter_Tk() if not self.gui["parent"] else tkinter_Toplevel(self.gui["parent"])
+        except Exception as e:
+            print(f"Scorpion: [ ERROR ] __load_gui(): Can't initialise tkinter: {e}")
+            return
+
+        self.gui["screen_width"] = self.gui["window"].winfo_screenwidth()
+        self.gui["screen_height"] = self.gui["window"].winfo_screenheight()
+
+        try:
+            self.__load_infos()
+        except Exception as e:
+            print(f"Scorpion: [ ERROR ] __load_gui():\nCan't load image infos: {e}")
+            return
+
+        try:
+            self.__load_img()
+        except Exception as e:
+            print(f"Scorpion: [ ERROR ] __load_gui():\nCan't load image: {e}")
+            return
+
+        self.gui["window"].title("S C O R P I O N")
+        self.gui["window"].mainloop()
+
+    def __load_infos(self):
+        frame_width = 400
+        frame_height = self.height if self.height < self.gui["screen_height"] - 150 else self.gui["screen_height"] - 150
+
+        base_frame = tkinter_Frame(self.gui["window"])
+        base_frame.pack(side="left")
+
+        x_scrollbar = tkinter_Scrollbar(base_frame, orient="horizontal")
+        x_scrollbar.pack(side="bottom", fill="x")
+        y_scrollbar = tkinter_Scrollbar(base_frame, orient="vertical")
+        y_scrollbar.pack(side="right", fill="y")
+
+        base_canvas = tkinter_Canvas(base_frame, width=frame_width, height=frame_height)
+        base_canvas.config(xscrollcommand=x_scrollbar.set, yscrollcommand=y_scrollbar.set)
+        base_canvas.pack(side="left", fill="y")
+
+        x_scrollbar.config(command=base_canvas.xview)
+        y_scrollbar.config(command=base_canvas.yview)
+
+        content_frame = tkinter_Frame(base_canvas)
+        base_canvas.create_window((0, 0), window=content_frame, anchor="nw")
+
+        filename_label = tkinter_Label(content_frame, text=f"Filename: {os_path_basename(self.path)}", justify="left")
+        filename_label.pack(anchor="w")
+        width_label = tkinter_Label(content_frame, text=f"Width: {self.width}", justify="left")
+        width_label.pack(anchor="w")
+        height_label = tkinter_Label(content_frame, text=f"Height: {self.height}", justify="left")
+        height_label.pack(anchor="w")
+
+        if len(self.exif):
+            exif_label = tkinter_Label(content_frame, text=f"--------- E X I F ---------", justify="left")
+            exif_label.pack(anchor="w")
+            for x, y in self.exif.items():
+                data_label = tkinter_Label(content_frame, text=f"{x}: {y}", justify="left")
+                data_label.pack(anchor="w")
+
+        if len(self.iptc):
+            iptc_label = tkinter_Label(content_frame, text=f"--------- I P T C ---------", justify="left")
+            iptc_label.pack(anchor="w")
+            for x, y in self.iptc.items():
+                data_label = tkinter_Label(content_frame, text=f"{x}: {y}", justify="left")
+                data_label.pack(anchor="w")
+
+        if len(self.xmp):
+            xmp_label = tkinter_Label(content_frame, text=f"---------- X M P ----------", justify="left")
+            xmp_label.pack(anchor="w")
+            for x, y in self.xmp.items():
+                data_label = tkinter_Label(content_frame, text=f"{x}: {y}", justify="left")
+                data_label.pack(anchor="w")
+
+        content_frame.update_idletasks()
+        base_canvas.config(scrollregion=base_canvas.bbox("all"))
+
+    def __load_img(self):
+        frame_width = self.width if self.width < self.gui["screen_width"] - 500 else self.gui["screen_width"] - 500
+        frame_height = self.height if self.height < self.gui["screen_height"] - 150 else self.gui["screen_height"] - 150
+
+        base_frame = tkinter_Frame(self.gui["window"])
+        base_frame.pack(side="left")
+
+        x_scroll = tkinter_Scrollbar(base_frame, orient="horizontal")
+        x_scroll.pack(side="bottom", fill="x")
+        y_scroll = tkinter_Scrollbar(base_frame, orient="vertical")
+        y_scroll.pack(side="right", fill="y")
+
+        base_canvas = tkinter_Canvas(base_frame, width=frame_width, height=frame_height, scrollregion=(0, 0, self.width, self.height))
+        base_canvas.config(xscrollcommand=x_scroll.set, yscrollcommand=y_scroll.set)
+        base_canvas.pack(side="left")
+
+        x_scroll.config(command=base_canvas.xview)
+        y_scroll.config(command=base_canvas.yview)
+
+        img = PIL_Image.open(self.path)
+        if self.format == "GIF":
+            frames = [PIL_ImageTk_PhotoImage(frame) for frame in PIL_ImageSequence_Iterator(img)]
+            delay = img.info.get("duration", 50)
+            img.close()
+            self.__update_frame(base_canvas, frames, len(frames), delay, 0)
+        else:
+            self.gui["img"] = PIL_ImageTk_PhotoImage(img)
+            img.close()
+            base_canvas.create_image(0, 0, anchor="nw", image=self.gui["img"])
+
+    def __update_frame(self, canvas: tkinter_Canvas, frames: list, count: int, delay: int, index: int):
+        if self.gui["img"]:
+            canvas.delete(self.gui["img"])
+        self.gui["img"] = canvas.create_image(0, 0, anchor="nw", image=frames[index])
+
+        self.gui["window"].after(delay, self.__update_frame, canvas, frames, count, delay, (index + 1) % count)
